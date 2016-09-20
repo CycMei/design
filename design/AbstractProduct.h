5aeaee6f1e731ed22adf041b1ac98ad19d0b5bdf
CycMei <chayalikemei@outlook.com>
2016-09-20 21:03:08 +0800

    night。。。。
    。。。。。。。。。。。。。。

---
+++ design/AbstractProduct.h
@@ -0,0 +1,40 @@
+#ifndef ABSTRACTTPRODUCT_H
+#define ABSTRACTTPRODUCT_H
+class AbstractProductA {
+public:
+	virtual ~AbstractProductA()=0;
+protected:
+	AbstractProductA();
+};
+class ProductA1 :public AbstractProductA {
+public:
+	ProductA1();
+	~ProductA1();
+};
+class ProductA2 :public AbstractProductA {
+public:
+	ProductA2();
+	~ProductA2();
+};
+
+
+
+
+class AbstractProductB {
+public:
+	virtual ~AbstractProductB();
+protected:
+	AbstractProductB();
+};
+class ProductB1 :public AbstractProductB {
+public:
+	ProductB1();
+	~ProductB1();
+};
+class ProductB2 :public AbstractProductB {
+public:
+	ProductB2();
+	~ProductB2();
+};
+
+#endif
--- design/main.cpp
+++ design/main.cpp
@@ -2,16 +2,29 @@
 
 #include"Factory.h"
 #include"Product.h"
+#include"AbstractFactory.h"
 #include<vld.h>
 #include<iostream>
 void test1() {
 	Factory *fac = new ConcreteFactory();
 	Product *pro = fac->CreateProduct();
-	delete fac;
-	fac = nullptr;
 	delete pro;
 	pro = nullptr;
+	delete fac;
+	fac = nullptr;
+}
+void test3() {
+	AbstractFactory *cf1 = new ConcreteFactory1();
+	auto A=cf1->CreateProductA();
+	auto B=cf1->CreateProductB();
+	delete A;
+	delete B;
+	delete cf1;
+
+	//AbstractFactory *cf2 = new ConcreteFactory2();
+	//cf2->CreateProductA();
+	//cf2->CreateProductB();
 }
 void main() {
-	test1();
+	test3();
 }  
